684931485f8ab74cabb2e779d175d7847d8d4a24
Ekaterina Semenova <panda_ket@bk.ru>
2016-04-17 19:03:39 +0300

    changes in constitution

--- Текущая версия/assets/js/constitutionTat.js
+++ Текущая версия/assets/js/constitutionTat.js
@@ -1,4 +1,54 @@
-$(function() {
-      $('html').addClass($.fn.details.support ? 'details' : 'no-details');
-      $('details').details();
-    });
+function createRequest()
+            {
+                var request = null;
+                try
+                {
+                    request = new XMLHttpRequest ();
+                }
+                catch (e)
+                {
+                    try
+                    {
+                        request = new ActiveXObject ("Microsoft.XMLHTTP");
+                    }
+                    catch (e) {}
+                }
+                return request;
+            }
+
+        function getFile (name)
+            {
+
+                var req = createRequest ();
+                req.open('GET', name, false);
+            //	req.overrideMimeType('text/plain; charset=windows-1251');
+                req.send(null);
+                document.getElementById ('mytext').firstChild.data = req.responseText;
+
+            }
+$(document).ready(function(){
+             $('.spoiler_links').click(function(){
+              $(this).parent().children('div.spoiler_body').toggle('normal');
+              return false;
+             });
+            });
+ $(function(){
+              $.fn.scrollToTop=function(){
+                $(this).hide().removeAttr("href");
+                if($(window).scrollTop()!="0"){
+                    $(this).fadeIn("slow")
+              }
+              var scrollDiv=$(this);
+              $(window).scroll(function(){
+                if($(window).scrollTop()=="0"){
+                $(scrollDiv).fadeOut("slow")
+                }else{
+                $(scrollDiv).fadeIn("slow")
+              }
+              });
+                $(this).click(function(){
+                  $("html, body").animate({scrollTop:0},"slow")
+                })
+              }
+            });
+            $(function() {$("#toTop").scrollToTop();});
